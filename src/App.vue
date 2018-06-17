41f634f1f94cfa8296d9819e8419f23046b120f4
agubelu <agustinbd3@gmail.com>
2018-06-17 20:52:55 +0200

    Canvas space and footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
     <div id="app">
         <ribbon color="red">Work in progress</ribbon>
+
+        <section class="hero is-primary">
+            <div class="hero-body" id="canvas-hero">
+                <div class="container">
+                    <canvas id="layer-dummy" class="canvas-layer" width="1354" height="160" style="z-index: 0;"/>
+                    <canvas id="layer0" class="canvas-layer canvas-drawable" width="1354" height="160" style="z-index: 0;"/>
+                    <canvas id="layer1" class="canvas-layer canvas-drawable" width="1354" height="160" style="z-index: 1;"/>
+                    <canvas id="layer2" class="canvas-layer canvas-drawable" width="1354" height="160" style="z-index: 2;"/>
+                </div>
+            </div>
+        </section>
+
         <img src="./assets/images/dog.gif">
         <h1>{{ msg }}</h1>
         <text-input :label="msg" v-model="msg" :isloading="isLoading"
         @focus="isLoading = true" @blur="isLoading = false"/>
+
+        <page-footer/>
     </div>
 </template>
 
 <script>
-import Ribbon from './components/Ribbon.vue';
-import TextInput from './components/TextInput.vue';
+import Ribbon from './components/Ribbon.vue'
+import TextInput from './components/TextInput.vue'
+import PageFooter from './components/Footer.vue'
 
 export default {
     name: "app",
@@ -22,14 +37,47 @@ export default {
         };
     },
 
-    components: { Ribbon, TextInput },
+    components: { Ribbon, TextInput, PageFooter },
+
+    mounted() {
+        function drawcircle(context, centerX, centerY, radius, color) {
+            context.beginPath();
+            context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+            context.fillStyle = color;
+            context.fill();
+        }
+
+        for(var i = 0; i < 3; i++) {
+            let color = i == 0 ? 'black' : i == 1 ? 'red' : 'blue';
+            var ctx = document.getElementById(`layer${i}`).getContext('2d');
+            drawcircle(ctx, 200+i*20, 70, 50, color);
+        }
+    }
 };
+
+
 </script>
 
 <style>
 #app {
     font-family: Helvetica, Arial, sans-serif;
     text-align: center;
-    margin-top: 60px;
+}
+
+#canvas-hero {
+    padding: .7%;
+    /*background-color: #fff;*/
+}
+
+.canvas-layer {
+    border: 2px dotted red;
+    max-width: 100%;
+    height: auto;
+}
+
+.canvas-drawable {
+    position: absolute;
+    left: 0;
+    top: 0;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+    <footer class="footer" id="footer">
+        <div class="columns">
+            <div class="column">
+                <p>
+                    <span class="pointer" @click="openTab('https://twitter.com/agubelu')">
+                        <i class="fab fa-twitter"></i> <span class="underline">Twitter</span>
+                    </span>
+                    &nbsp;|&nbsp;
+                    <span class="pointer" @click="openTab('https://osu.ppy.sh/forum/ucp.php?i=pm&mode=compose&u=4780236')">
+                        <i class="fa fa-envelope"></i> <span class="underline">Contact me</span>
+                    </span>
+                </p>
+            </div>
+            <div class="column">
+                <div class="content has-text-centered">
+                    <p>Made with ❤️ by agubelu</p>
+                </div>
+            </div>
+            <div class="column">
+                <p>
+                    <span class="pointer" @click="openTab('https://github.com/agubelu/osu-healthbar-gen')">
+                        <span class="underline">View on GitHub</span> <i class="fab fa-github"></i>
+                    </span>
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+export default {
+
+    components: { FontAwesomeIcon },
+
+    methods: {
+        openTab(url) {
+            window.open(url, '_blank');
+        }
+    }
+
+}
+</script>
+
+<style>
+#footer {
+    background-color: #e0e0e0;
+    padding: 1%;
+}
+
+#footer > .container {
+    max-width: 1500px !important;
+}
+
+.pointer {
+    cursor: pointer;
+}
+
+.underline {
+    text-decoration: underline;
+}
+</style>
