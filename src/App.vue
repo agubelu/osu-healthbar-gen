d851e965f50edfe0097d743bd01c64af4cbc4fe0
agubelu <agustinbd3@gmail.com>
2018-06-17 19:14:27 +0200

    Added text-input component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,26 @@
         <ribbon color="red">Work in progress</ribbon>
         <img src="./assets/images/dog.gif">
         <h1>{{ msg }}</h1>
+        <text-input :label="msg" v-model="msg" :isloading="isLoading"
+        @focus="isLoading = true" @blur="isLoading = false"/>
     </div>
 </template>
 
 <script>
 import Ribbon from './components/Ribbon.vue';
+import TextInput from './components/TextInput.vue';
 
 export default {
     name: "app",
 
     data() {
         return {
-            msg: "WIP"
+            msg: "WIP",
+            isLoading: false,
         };
     },
 
-    components: { Ribbon },
+    components: { Ribbon, TextInput },
 };
 </script>
 
---
+++ src/components/TextInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="field">
+        <label v-if="label" class="label">{{ label }}</label>
+        <div :class="{ 'control': true, 'has-icons-left': iconleft, 'has-icons-right': iconright, 'is-loading': isloading }">
+            <input :class="{ 'input': true, 'is-danger': error }" type="text" 
+                    :placeholder="placeholder" @focus="sendOnFocus" @blur="sendOnBlur"
+                    :value="value" @input="$emit('input', $event.target.value)">
+            <span v-if="iconleft" class="icon is-small is-left">
+                <font-awesome-icon :icon="iconleft" />
+            </span>
+            <span v-if="iconright" class="icon is-small is-right">
+                <font-awesome-icon :icon="iconright" />
+            </span>
+        </div>
+        <p v-if="error" class="help is-danger">{{ error }}</p>
+    </div>
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+export default {
+    props: {
+        label: { default: '' },
+        placeholder: { default: '' },
+        iconleft: { default: '' },
+        iconright: { default: '' },
+        isloading: { default: '' },
+        value: { default: '' },
+        error: { default: '' },
+    },
+
+    methods: {
+        sendOnFocus() {
+            this.$emit('focus', this);
+        },
+
+        sendOnBlur() {
+            this.$emit('blur', this);
+        },
+    },
+
+    components: { FontAwesomeIcon },
+
+}
+</script>
+
+<style>
+
+</style>
